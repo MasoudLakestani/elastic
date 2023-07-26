306d6512a237d1add234bbb8f73aefaef540556c
masoud89git config --global user.email m.lakestani89@gmail.com <m.lakestani89@gmail.comgit config --global user.name masoud89>
2023-07-26 15:14:24 +0330

    create ELK project

---
+++ create_mapping.py
@@ -0,0 +1,79 @@
+from elasticsearch import Elasticsearch
+import time
+
+# Connect to Elasticsearch
+es = Elasticsearch("http://localhost:9200")
+
+# Ensure Elasticsearch is up
+while True:
+    try:
+        if es.ping():
+            print("Connected to Elasticsearch.")
+            break
+    except Exception as e:
+        print(f"Waiting for Elasticsearch... Error: {e}")
+        time.sleep(5)
+
+settings = {
+    "settings": {
+        "analysis": {
+            "char_filter": {
+                "zero_width_spaces": {
+                    "type": "mapping",
+                    "mappings": ["\\u200C => \\u0020"]
+                }
+            },
+            "filter": {
+                "persian_stop": {
+                    "type": "stop",
+                    "stopwords": "_persian_"
+                },
+                "english_stop": {
+                    "type": "stop",
+                    "stopwords": "_english_"
+                },
+                "synonym_words":{
+                    "type": "synonym",
+                    "synonyms_path": "/usr/share/elasticsearch/config/synonyms/synonym.txt"
+                }
+            },
+            "analyzer": {
+                "persian_analyzer": {
+                    "tokenizer": "standard",
+                    "char_filter": ["zero_width_spaces"],
+                    "filter": [
+                        "lowercase",
+                        "decimal_digit",
+                        "arabic_normalization",
+                        "persian_normalization",
+                        "persian_stop",
+                        "synonym_words",
+                        "unique"
+                    ]
+                },
+                "english_analyzer":{
+                    "tokenizer": "standard",
+                    "filter": [
+                        "lowercase",
+                        "english_stop",
+                        "unique",
+                        "synonym_words",
+                    ]   
+
+                }
+            }
+        }
+    },
+    "mappings": {
+        "properties": {
+            "id" : { "type": "integer" },
+            "title_fa" : { "type": "text", "analyzer": "persian_analyzer" },
+            "title_en" : {"type": "text", "analyzer": "english_analyzer" },
+            "category" : { "type": "text", "analyzer": "persian_analyzer" },
+            "url" : { "enabled": "false" },
+            "is_active" : { "enabled": "false" },
+        }
+    }
+}
+
+es.indices.create(index="blackeveryday_product", body=settings, ignore=400)
