7587a2d2218397d9e7f51a359398178cc54e1d1f
masoud89git config --global user.email m.lakestani89@gmail.com <m.lakestani89@gmail.comgit config --global user.name masoud89>
2023-08-20 17:22:06 +0330

    add sql_last_value to logstash.conf

--- create_mapping.py
+++ create_mapping.py
@@ -14,75 +14,6 @@ while True:
     except Exception as e:
         print(f"Waiting for Elasticsearch... Error: {e}")
         time.sleep(5)
-
-# settings = {
-#     "settings": {
-#         "analysis": {
-#             "char_filter": {
-#                 "zero_width_spaces": {
-#                     "type": "mapping",
-#                     "mappings": ["\\u200C => \\u0020"]
-#                 }
-#             },
-#             "filter": {
-#                 "persian_stop": {
-#                     "type": "stop",
-#                     "stopwords": "_persian_"
-#                 },
-#                 "english_stop": {
-#                     "type": "stop",
-#                     "stopwords": "_english_"
-#                 },
-#                 "synonym_words":{
-#                     "type": "synonym",
-#                     "synonyms_path": "/usr/share/elasticsearch/config/synonyms/synonym.txt"
-#                 }
-#             },
-#             "analyzer": {
-#                 "persian_analyzer": {
-#                     "tokenizer": "standard",
-#                     "char_filter": ["zero_width_spaces"],
-#                     "filter": [
-#                         "lowercase",
-#                         "decimal_digit",
-#                         "arabic_normalization",
-#                         "persian_normalization",
-#                         "persian_stop",
-#                         "synonym_words",
-#                         "unique"
-#                     ]
-#                 },
-#                 "english_analyzer":{
-#                     "tokenizer": "standard",
-#                     "filter": [
-#                         "lowercase",
-#                         "english_stop",
-#                         "unique",
-#                         "synonym_words",
-#                     ]   
-
-#                 }
-#             }
-#         }
-#     },
-#     "mappings": {
-#         "properties": {
-#             "id" : { "type": "integer" },
-#             "title_fa" : { "type": "text", "analyzer": "persian_analyzer" },
-#             "title_en" : {"type": "text", "analyzer": "english_analyzer" },
-#             "category1" : { "type": "text", "analyzer": "persian_analyzer" },
-#             "website" : { "enabled": "false" },
-#             "website_url" :{ "enabled": "false" },
-#             "url" : { "enabled": "false" },
-#             "is_active" : { "type": "boolean"},
-#             "image" : { "enabled": "false" },
-#             "discount_price" : { "type": "float" },
-#             "selling_price" : { "type": "float" },
-#             "rrp_price" : { "type": "float" },
-#             "discount_percent" : { "type": "float" },
-#         }
-#     }
-# }
 settings = {
     "settings": {
         "number_of_shards": 1,
@@ -170,7 +101,8 @@ settings = {
             "brand":{"type":"text", "analyzer":"persian_analyzer"},
             "website" : { "enabled": "false" },
             "website_url" :{ "enabled": "false" },
-            "url" : { "enabled": "false" },
+            "url" : { "enabled": "false"},
+            "write_date" : {"enabled": "false"},
             "is_active" : { "type": "boolean"},
             "image" : { "enabled": "false" },
             "discount_price" : { "type": "float" },
